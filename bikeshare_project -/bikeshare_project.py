c7410e39eab4a93c1d07cddd3ffa2742516b74a7
Abdulaziz-Alfahmi <Abdulaziz.s.f2016@gmail.com>
2021-08-07 16:25:20 +0300

    Feat: Adding while loop to handle wrong inputs in raw data function

--- bikeshare_project -/bikeshare_project.py
+++ bikeshare_project -/bikeshare_project.py
@@ -8,7 +8,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
 
 
 def get_filters():
-            ''' Asking the user to insert the input of the filter '''
+    # ''' Asking the user to insert the input of the filter '''
 
     print('Hello! Let\'s explore some US bikeshare data!')
     while True:
@@ -70,7 +70,7 @@ def get_filters():
 
 
 def load_data(city, month, day):
-            '''Filtering the data based on the user inputs '''
+    # '''Filtering the data based on the user inputs '''
     df = pd.read_csv(CITY_DATA[city])
 
     df['Start Time'] = pd.to_datetime(df['Start Time'])
@@ -140,7 +140,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-                """Displays statistics on bikeshare users."""
+    # """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
@@ -165,22 +165,29 @@ def user_stats(df):
 
     print('-'*40)
 
-def rawdata(df):
-                '''Displays the raw data , five rows at a time '''
 
-    x = input('If you want to see 5 rows of the raw data write "yes" , otherwise write "no"\n')
-    y = 0
-    z = 5
-    while x == 'yes':
-        print(df.iloc[y:z])
-        y += 5
-        z += 5
-        x = input('if you eant to see more 5 rows of the raw data enter "yes" , otherwise write  "no" \n')
+def rawdata(df):
+    #'''Displays the raw data , five rows at a time '''
+    while True:
+        x = input('If you want to see 5 rows of the raw data write "yes" , otherwise write "no"\n').lower()
+        y = 0
+        z = 5
+        if x in ['yes', 'no']:
+            break
+        print('Ops invalid entry , please try again')
+    if x == 'yes':
+        while True:
+            print(df.iloc[y:z])
+            y += 5
+            z += 5
+            x = input('if you eant to see more 5 rows of the raw data enter "yes" , otherwise write  "no" \n')
+            if x in ['yes', 'no']:
+                break
+            print('Ops invalid entry , please try again')
 
 
 def main():
     while True:
-
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
